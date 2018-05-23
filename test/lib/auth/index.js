edea844cd2184c8f1e7ed06ba9bc2c221502bc27
ReinsBrain <email@reinpetersen.com>
2018-05-23 17:16:37 -0500

    convert Auth to es6 class for easy extensibility

--- test/lib/auth/index.js
+++ test/lib/auth/index.js
@@ -11,47 +11,51 @@ const MEREQ = require('../../mock/express/request.js');
 const JWT = require('jwt-simple');
 let ttl = new Date();
 ttl.setHours(ttl.getHours() + 1);
-const user_jwt = JWT.encode(
-    {
-        'exp': ttl.getTime(),
-        'user': {
-            'id': '54630ce6c223d83494e2d776',
-            'roles': [
-                '507f191e810c19729de860ea',
-                '54759eb3c090d83494e2d804',
-                '57654cf3c123d65931e4f678'
-            ],
-            'name': 'super',
-            'email': 'mocha@rebel.io'
-        }
-    },
-    process.env.JWT_SECRET
-);
-const jwt_check = JWT.decode(user_jwt, process.env.JWT_SECRET);
 
 const Auth = require('../../../lib/auth');
 
 describe('lib/auth/auth.js', function () {
-    describe('#generateAuthMiddleWare', function () {
-        const auth = new Auth(process.env.JWT_SECRET, {});
-        it('should return an function for use as authentication middleware',
-            function () {
-                const headers = { 'authorization': 'Bearer ' + user_jwt };
-                const req = MEREQ(headers);
-                const res = new (require('../../mock/express/response.js'))();
 
-                const func_authChecker = auth.generateAuthMiddleWare();
+	describe('$decodeJWT', function(){
+		it('should return correctly decoded', function(){
+			let token = {'exp':ttl.getTime(),'payload':{'a':1}};
+			let testencode = JWT.encode(token,process.env.JWT_SECRET);
+			let testdecode = Auth.decodeJWT(testencode, process.env.JWT_SECRET);
+			expect(testdecode).to.deep.equal(token);
+		});
+	});
 
-                assert.equal(func_authChecker.length, 3, 'Middleware should expect three arguments');
-            });
-    });
-    // describe('method Process', function() {});
+	describe('$encodeJWT', function(){
+		it('should return correctly decoded', function(){
+			let token = {'exp':ttl.getTime(),'payload':{'a':1}};
+			let testencode = Auth.encodeJWT(token,process.env.JWT_SECRET);
+			expect(testencode).to.deep.equal(JWT.encode(token, process.env.JWT_SECRET));
+		});
+	});
+
+
+	describe('#constructor', function () {
+		const auth = new Auth(process.env.JWT_SECRET, {});
+		it('should return instanceof Auth', function(){
+			expect(auth).to.be.instanceof(Auth);	
+		});
+	});
+
+	describe('#generateAuthMiddleWare', function () {
+		const auth = new Auth(process.env.JWT_SECRET, {});
+		const user_jwt = JWT.encode({'exp': ttl.getTime(), 'user':{'id':'123'}}, process.env.JWT_SECRET );
+		
+		it('should return an function for use as authentication middleware',
+			function () {
+				const headers = { 'authorization': 'Bearer ' + user_jwt };
+				const req = MEREQ(headers);
+				const res = new (require('../../mock/express/response.js'))();
+
+				const func_authChecker = auth.generateAuthMiddleWare();
+
+				assert.equal(func_authChecker.length, 3, 'Middleware should expect three arguments');
+			}
+		);
+	});
+
 });
-
-// func_authChecker( req, res, function(){
-
-//     console.log( req.dtoken )
-
-//     done();
-
-// } );
