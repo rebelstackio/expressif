4bbcd6945b586651175d6fdcdd8ee8d4fa29ce70
Javier Galarza <jegj57@gmail.com>
2019-05-03 14:05:51 -0500

    Update test based on the new name

--- test/lib/error/index.spec.js
+++ test/lib/error/index.spec.js
@@ -13,55 +13,55 @@ const ErrorHttpCodes= require('lib/error/httpcodes');
 
 describe.only('lib/error/index.js',  () => {
 
-	describe('#AppError', () => {
-		let AppError;
+	describe('#ExpError', () => {
+		let ExpError;
 
 		beforeEach(() => {
-			AppError = Errors.AppError;
+			ExpError = Errors.ExpError;
 		});
 
-		it('Should the AppError be available in the lib/error module', () => {
-			expect(Errors).to.has.property('AppError');
+		it('Should the ExpError be available in the lib/error module', () => {
+			expect(Errors).to.has.property('ExpError');
 		});
 
-		it('Should AppError object be an instance of Error', () => {
-			const err = new AppError(null, null, 'sample error');
+		it('Should ExpError object be an instance of Error', () => {
+			const err = new ExpError(null, null, 'sample error');
 			expect(err).to.be.instanceOf(Error);
 		});
 
-		it('Should AppError has the property message', () => {
+		it('Should ExpError has the property message', () => {
 			const msg = 'sample error';
-			const err = new AppError(ErrorTypes.serverError, ErrorHttpCodes.serverError, msg);
+			const err = new ExpError(ErrorTypes.serverError, ErrorHttpCodes.serverError, msg);
 			expect(err).to.has.property('message', msg);
 		});
 
-		it('Should AppError has the type property by default as Server Error', () => {
+		it('Should ExpError has the type property by default as Server Error', () => {
 			const msg = 'sample error';
-			const err = new AppError(null, null, msg);
+			const err = new ExpError(null, null, msg);
 			expect(err).to.has.property('type', ErrorTypes.serverError);
 		});
 
-		it('Should be possible to set the type property in an AppError', () => {
+		it('Should be possible to set the type property in an ExpError', () => {
 			const msg = 'sample error';
-			const err = new AppError(ErrorTypes.badRequest, null, msg);
+			const err = new ExpError(ErrorTypes.badRequest, null, msg);
 			expect(err).to.has.property('type', ErrorTypes.badRequest);
 		});
 
-		it('Should AppError has the httpstatus property by default as Server Error(500)', () => {
+		it('Should ExpError has the httpstatus property by default as Server Error(500)', () => {
 			const msg = 'sample error';
-			const err = new AppError(null, null, msg);
+			const err = new ExpError(null, null, msg);
 			expect(err).to.has.property('httpstatus', ErrorHttpCodes.serverError);
 		});
 
-		it('Should be possible to set the httpstatus property in an AppError', () => {
+		it('Should be possible to set the httpstatus property in an ExpError', () => {
 			const msg = 'sample error';
-			const err = new AppError(null, ErrorHttpCodes.badRequest, msg);
+			const err = new ExpError(null, ErrorHttpCodes.badRequest, msg);
 			expect(err).to.has.property('httpstatus', ErrorHttpCodes.badRequest);
 		});
 
-		it('Should AppError has a errorObject property by defaul as an empty object', () => {
+		it('Should ExpError has a errorObject property by defaul as an empty object', () => {
 			const msg = 'sample error';
-			const error = new AppError(ErrorTypes.badRequest, ErrorHttpCodes.serverError, msg );
+			const error = new ExpError(ErrorTypes.badRequest, ErrorHttpCodes.serverError, msg );
 			expect(error).to.has.property('errorObject');
 			expect(error.errorObject).to.be.deep.equal({});
 		});
@@ -69,7 +69,7 @@ describe.only('lib/error/index.js',  () => {
 		it('Should be possible to set an errorObject( error from another source) in the options argument', () => {
 			const msg = 'sample error';
 			const err = new Error('error');
-			const error = new AppError(ErrorTypes.badRequest, ErrorHttpCodes.serverError, msg, { errorObject: err } );
+			const error = new ExpError(ErrorTypes.badRequest, ErrorHttpCodes.serverError, msg, { errorObject: err } );
 			expect(error).to.has.property('errorObject');
 			expect(error.errorObject).to.be.deep.equal(err);
 		});
@@ -78,17 +78,17 @@ describe.only('lib/error/index.js',  () => {
 			const msg = 'sample error';
 			const err = new Error('error');
 			const options = { errorObject: err, important: true, access: true };
-			const error = new AppError(ErrorTypes.badRequest, ErrorHttpCodes.serverError, msg, options );
+			const error = new ExpError(ErrorTypes.badRequest, ErrorHttpCodes.serverError, msg, options );
 			delete options.errorObject;
 			expect(error).to.has.property('props');
 			expect(error.props).to.be.deep.equal(options);
 		});
 
-		it('Should an AppError display a string message when it is represented as string', () => {
+		it('Should an ExpError display a string message when it is represented as string', () => {
 			const msg = 'sample error';
 			const err = new Error('error');
 			const options = { errorObject: err, important: true, access: true };
-			const error = new AppError(ErrorTypes.badRequest, ErrorHttpCodes.badRequest, msg, options );
+			const error = new ExpError(ErrorTypes.badRequest, ErrorHttpCodes.badRequest, msg, options );
 			expect(error.toString()).to.be.equal(`[${ErrorTypes.badRequest}|${ErrorHttpCodes.badRequest}] ${msg}`);
 		});
 
