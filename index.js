c946d569c6ce3165740973fec69c01b8e41e8314
Javier Galarza <jegj57@gmail.com>
2019-05-10 15:04:46 -0500

    Re organize expError object inside a ExpObject folder

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const { Auth, AuthByPrivs } = require('./lib/auth');
 
 const DB = require('./lib/db/pg');
 
-const { ExpError, EXPRESSIF_HTTP_CODES, EXPRESSIF_HTTP_TYPES } = require('./lib/error');
-
 const Exception = require('./lib/exception');
 
 const JSONValidator = require('./lib/jsonvalidator');
@@ -30,8 +28,5 @@ module.exports = {
 	ReqValidator,
 	Router,
 	Server,
-	BasicLogger,
-	ExpError,
-	EXPRESSIF_HTTP_CODES,
-	EXPRESSIF_HTTP_TYPES
+	BasicLogger
 };
