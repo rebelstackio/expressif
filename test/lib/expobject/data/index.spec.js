ee8ffcd78da9d2735d2da475ce583964c3afe3c6
Javier Galarza <jegj57@gmail.com>
2019-05-14 11:08:00 -0500

    Update test and expData class with the json method

--- test/lib/expobject/data/index.spec.js
+++ test/lib/expobject/data/index.spec.js
@@ -57,20 +57,20 @@ describe('lib/expobject/data/index.js',  () => {
 		});
 
 		it('Should be possible to set the httpstatus property based on random code', () => {
-			const response = new ExpData(HttpTypes.noContent, 400.11);
-			expect(response).to.has.property('httpstatus', 400.11);
+			const response = new ExpData(HttpTypes.noContent, 200.11);
+			expect(response).to.has.property('httpstatus', 200.11);
 		});
 
 		it('Should set the props property with custom properties', () => {
 			const props = { a:1 , b:2, c:3 };
-			const response = new ExpData(HttpTypes.noContent, 400.11, {}, props);
+			const response = new ExpData(HttpTypes.noContent, HttpCodes.noContent, {}, props);
 			expect(response).to.has.property('props');
 			expect(response.props).to.be.deep.equal(props);
 		});
 
 		it('Should set the props property with custom properties and the adheaders', () => {
 			const props = { a:1 , b:2, c:3, adheaders: { header1: 1, header2: 2} };
-			const response = new ExpData(HttpTypes.noContent, 400.11, {}, props);
+			const response = new ExpData(HttpTypes.noContent, HttpCodes.noContent, {}, props);
 			expect(response).to.has.property('props');
 			expect(response.props).to.be.deep.equal({ a:1 , b:2, c:3 });
 			expect(response.adheaders).to.be.deep.equal(props.adheaders);
@@ -78,14 +78,27 @@ describe('lib/expobject/data/index.js',  () => {
 
 		it('Should hasAdditionalHeaders return true when the adheaders option is valid', () => {
 			const props = { a:1 , b:2, c:3, adheaders: { header1: 1, header2: 2} };
-			const response = new ExpData(HttpTypes.noContent, 400.11, {}, props);
+			const response = new ExpData(HttpTypes.noContent, HttpCodes.noContent, {}, props);
 			expect(response.hasAdditionalHeaders).to.be.true;
 		});
 
 		it('Should hasAdditionalHeaders return false when the are not adheader in the options object', () => {
 			const props = { a:1 , b:2, c:3 };
-			const response = new ExpData(HttpTypes.noContent, 400.11, {}, props);
+			const response = new ExpData(HttpTypes.noContent, HttpCodes.noContent, {}, props);
 			expect(response.hasAdditionalHeaders).to.be.false;
+		});
+
+		it('Should the json method return an object with the common properties for the expData instance', () => {
+			const props = { a:1 , b:2, c:3 };
+			const response = new ExpData(HttpTypes.noContent, HttpCodes.noContent, {arr: [1,2,3]}, props);
+			const json = response.json();
+			expect(json).to.be.an('object');
+			expect(json).to.has.property('httpstatus', HttpCodes.noContent)
+			expect(json).to.has.property('type', HttpTypes.noContent)
+			expect(json).to.has.property('data');
+			expect(json.data).to.be.deep.equal({arr: [1,2,3]});
+			expect(json).to.has.property('props');
+			expect(json.props).to.be.deep.equal(props);
 		});
 
 	});
