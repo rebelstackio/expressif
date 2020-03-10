8fe785913412ff49c99760fe3f149acbd4e428a6
Javier Galarza <jegj57@gmail.com>
2020-03-09 22:10:18 -0500

    Add new docs for server v2

--- testserver/routers/index.js
+++ testserver/routers/index.js
@@ -3,7 +3,7 @@
 function registerRouters( app ) {
 	app.use( '/v1/healthcheck',     require('./healthcheck')() );
 	app.use( '/v2/healthcheck',   require('./healthcheck/v2')() );
-};
+}
 
 module.exports = registerRouters;
 
