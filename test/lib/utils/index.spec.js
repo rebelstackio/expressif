30a7b248a700bf1559ce1086ce71b11c450c7028
Javier Galarza <jegj57@gmail.com>
2020-03-16 23:24:39 -0500

    Add collectFilePathsRecursive functions for get all the files under a folder recursively

--- test/lib/utils/index.spec.js
+++ test/lib/utils/index.spec.js
@@ -49,4 +49,125 @@ describe('lib/util/index.js', () => {
 
 	});
 
+	describe('collectFilePathsRecursive', () => {
+
+		let collectFilePathsRecursive,readdirSync, join, statSync;
+
+		beforeEach(() => {
+			collectFilePathsRecursive = Util.collectFilePathsRecursive;
+			readdirSync = jest.fn().mockImplementation(() => {
+				return [];
+			});
+			join = jest.fn();
+			statSync = jest.fn(() => {
+				return {
+					isDirectory: () => false
+				};
+			});
+		});
+
+		test('collectFilePathsRecursive return an empty array if the main folder is empty', () => {
+			const result = collectFilePathsRecursive('/home/nodeserver/schemas', [], { readdirSync, join, statSync });
+			expect(result).toBeArrayOfSize(0);
+		});
+
+		test('collectFilePathsRecursive return an array of all the absolute paths of the files', () => {
+			readdirSync = jest.fn().mockImplementation(() => {
+				return [
+					'file1.js',
+					'file2.js',
+					'file3.js'
+				];
+			});
+			join = require('path').join;
+
+			const result = collectFilePathsRecursive('/home/nodeserver/schemas', [], { readdirSync, join, statSync });
+
+			expect(result).toBeArrayOfSize(3);
+			expect(result[0]).toBe('/home/nodeserver/schemas/file1.js');
+			expect(result[1]).toBe('/home/nodeserver/schemas/file2.js');
+			expect(result[2]).toBe('/home/nodeserver/schemas/file3.js');
+		});
+
+		test('collectFilePathsRecursive must call it self recursive for nested folders', () => {
+			readdirSync = jest.fn().mockImplementation(( folder ) => {
+				if ( folder === '/home/nodeserver/schemas/folder1') {
+					return [
+						's1.json',
+						's2.json',
+						's3.json'
+					];
+				} else {
+					return [
+						'folder1',
+					];
+				}
+			});
+			join = require('path').join;
+			statSync = jest.fn((x) => {
+				if ( x === '/home/nodeserver/schemas/folder1') {
+					return {
+						isDirectory: () => true
+					};
+				} else {
+					return {
+						isDirectory: () => false
+					};
+				}
+			});
+
+			const result = collectFilePathsRecursive('/home/nodeserver/schemas', [], { readdirSync, join, statSync });
+
+			expect(result).toBeArrayOfSize(3);
+			expect(result[0]).toBe('/home/nodeserver/schemas/folder1/s1.json');
+			expect(result[1]).toBe('/home/nodeserver/schemas/folder1/s2.json');
+			expect(result[2]).toBe('/home/nodeserver/schemas/folder1/s3.json');
+		});
+
+
+		test('collectFilePathsRecursive must call it self recursive for nested folders and collect single files in the upper level', () => {
+			readdirSync = jest.fn().mockImplementation(( folder ) => {
+				if ( folder === '/home/nodeserver/schemas/folder1') {
+					return [
+						's1.json',
+						's2.json',
+						's3.json'
+					];
+				} else {
+					return [
+						'folder1',
+						'test1.json',
+						'test2.json'
+					];
+				}
+			});
+			join = require('path').join;
+			statSync = jest.fn((x) => {
+				if ( x === '/home/nodeserver/schemas/folder1') {
+					return {
+						isDirectory: () => true
+					};
+				} else {
+					return {
+						isDirectory: () => false
+					};
+				}
+			});
+
+			const result = collectFilePathsRecursive('/home/nodeserver/schemas', [], { readdirSync, join, statSync });
+
+			expect(result).toBeArrayOfSize(5);
+			expect(result).toIncludeAnyMembers([
+				'/home/nodeserver/schemas/folder1/s1.json',
+				'/home/nodeserver/schemas/folder1/s2.json',
+				'/home/nodeserver/schemas/folder1/s3.json',
+				'/home/nodeserver/schemas/test1.json',
+				'/home/nodeserver/schemas/test2.json'
+			]);
+		});
+
+	});
+
 });
+
+
